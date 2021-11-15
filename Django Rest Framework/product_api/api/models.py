8892b363884a0d59f6545810dc0b5d11686c12f0
Shabbir1603100 <56122154+Shabbir1603100@users.noreply.github.com>
2021-11-15 19:35:02 +0600

    Add files via upload

---
+++ Django Rest Framework/product_api/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    timingID = models.IntegerField()
+    timeFor = models.CharField(max_length=20, null=False, blank=False)
+    classesID = models.IntegerField()
+    sectionID = models.IntegerField()
+    entryFrom = models.DateTimeField(auto_now_add=True)
+    entryTo = models.DateTimeField(auto_now_add=True)
+    exitFrom = models.DateTimeField(auto_now_add=True)
+    exitTo = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.timingID
