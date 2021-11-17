5b69c8472cb7f8b0dede37c36a55341ed574fa9d
Shabbir1603100 <56122154+Shabbir1603100@users.noreply.github.com>
2021-11-18 01:27:08 +0600

    Update views.py

--- Django Rest Framework/product_api/api/views.py
+++ Django Rest Framework/product_api/api/views.py
@@ -1,6 +1,7 @@
+from django.db.migrations import serializer
 from django.shortcuts import render
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,69 +10,45 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from . import models
 from .serializers import AttSerializer
 from .models import Att
 import requests
 from rest_framework import status
 
+
 # Create your views here.
-
-"""@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'List': '/product-list/',
-        'Detail View': '/product-detail/<int:id>/',
-        'Create': '/product-create/',
-        'Update': '/product-update/<int:id>/',
-        'Delete': '/product-detail/<int:id>/',
-    }
-    return Response(api_urls);
-"""
-
-"""@api_view(['GET'])
+@api_view(['GET'])
 def ShowAll(request):
     atts = Att.objects.all()
     serializer = AttSerializer(atts, many=True)
     return Response(serializer.data)
-"""
 
 
-@api_view(['GET'])
-def ViewAtt(request, classesID=None):
-    class_id = request.query_params.get('classId', None)
-    if class_id is None:
-    #return Bad Request
-    result = Att.objects.filter(classesID=class_id)
-    # rests are same
+def _get_time_range(self):
+    url = "http://rc.touchtechbd.com/attendance_sync/getTiming/?classesID=2"
+    api_request = requests.get(url)
 
-    att = Att.objects.filter(classesID)
-    serializer = AttSerializer(att, many=True)
-    return Response(serializer.data)
+    try:
+        api_request.raise_for_status()
+        return api_request.json()
+    except:
+        return None
 
 
-@api_view(['POST'])
-def CreateAtt(request):
-
-    serializer = AttSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-
-    def deleteAtt(request, pk):
-        att = Att.objects.all()
-        att.delete()
-        return Response('Items delete successfully!')
-
-    return JsonResponse(serializer.errors, status=400)
+def save_data(self):
+    time_range = self._get_time_range()
+    if time_range is not None:
+        try:
+            url = "http://rc.touchtechbd.com/attendance_sync/Attendance"
+            _post_data = requests.post(url, data=models.Att)
+            time_range.save()
+        except:
+            pass
 
 
-"""@api_view(['POST'])
-def updateAtt(request, pk):
-    att = Att.objects.get(id=pk)
-    serializer = AttSerializer(instance=att, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-"""
+@api_view(['DELETE'])
+def deleteAtt(request, pk):
+    att = Att.objects.all()
+    att.delete()
+    return Response('Items delete successfully!')
