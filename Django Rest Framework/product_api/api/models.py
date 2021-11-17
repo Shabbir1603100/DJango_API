5a1b2a4af9cee0969808995bf1c0895e6731f174
Shabbir1603100 <56122154+Shabbir1603100@users.noreply.github.com>
2021-11-17 15:35:34 +0600

    Update models.py

--- Django Rest Framework/product_api/api/models.py
+++ Django Rest Framework/product_api/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -8,10 +9,7 @@ class Att(models.Model):
     timeFor = models.CharField(max_length=20, null=False, blank=False)
     classesID = models.IntegerField()
     sectionID = models.IntegerField()
-    entryFrom = models.DateTimeField(auto_now_add=True)
-    entryTo = models.DateTimeField(auto_now_add=True)
-    exitFrom = models.DateTimeField(auto_now_add=True)
-    exitTo = models.DateTimeField(auto_now_add=True)
+    entryTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.timingID
+        return self.timeFor
