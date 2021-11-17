96d68612437d3e354ce9450e2721625c3ff57356
Shabbir1603100 <56122154+Shabbir1603100@users.noreply.github.com>
2021-11-18 01:26:16 +0600

    Update urls.py

--- Django Rest Framework/product_api/api/urls.py
+++ Django Rest Framework/product_api/api/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.apiOverview, name='apiOverview'),
 
     path('att-list/', views.ShowAll, name='att-list'),
-    path('att-detail/<int:pk>/', views.ViewAtt, name='att-detail'),
-    path('att-create/', views.CreateAtt, name='att-create'),
-    path('att-update/<int:pk>/', views.updateAtt, name='att-update'),
+    # path('time-range/<int:pk>/', views.Viewtime, name='time-range'),
+    # path('att-create/', views.CreateAtt, name='att-create'),
     path('att-delete/<int:pk>/', views.deleteAtt, name='att-delete'),
 
 ]
