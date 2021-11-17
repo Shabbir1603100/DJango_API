ff4c77b314611acc781f653832c00ab883459787
Shabbir1603100 <56122154+Shabbir1603100@users.noreply.github.com>
2021-11-17 13:12:11 +0600

    Update models.py

--- Django Rest Framework/product_api/api/models.py
+++ Django Rest Framework/product_api/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Product(models.Model):
+class Att(models.Model):
     timingID = models.IntegerField()
     timeFor = models.CharField(max_length=20, null=False, blank=False)
     classesID = models.IntegerField()
